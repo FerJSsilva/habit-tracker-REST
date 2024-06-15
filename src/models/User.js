e0da8baa6b518899eb36f0a9ee14598f90be21ae
Fernando JS Silva <ferjssilva@gmail.com>
2024-06-15 20:31:03 -0300

    feature: disable get for users

--- src/models/User.js
+++ src/models/User.js
@@ -18,6 +18,15 @@ const UserSchema = new Schema({
   }
 });
 
+UserSchema.methods.apiValues = async function () {
+  throw new Error('GET is disabled for this route');
+};
+
+// schema.statics.apiPost DESABILITA POST
+// schema.methods.apiValues DESABILITA GET
+// schema.methods.apiPut DESABILITA PUT
+// schema.methods.apiDelete DESABILITA DELETE
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
