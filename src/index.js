0d622caa065284d752117a93b77e9adc8d3b13a9
Fernando JS Silva <ferjssilva@gmail.com>
2024-11-04 18:59:25 -0300

    feat: remove swagger

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import mongoose from 'mongoose';
 import { connectToDatabase } from './config/database.js';
 import preSerialization from './hooks/preSerialization.js';
 import fastifyMongooseAPI from 'fastify-mongoose-api';
-import swagger from '@fastify/swagger';
-import swaggerUi from '@fastify/swagger-ui';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -27,50 +25,6 @@ import './models/UserHabit.js';
 
 /* ------------------------------- Fastify Plugins ------------------------------ */
 
-// Swagger options
-const swaggerOptions = {
-  openapi: {
-    info: {
-      title: 'Habit Tracker API',
-      description: 'API documentation',
-      version: '0.1.0'
-    },
-    servers: [
-      {
-        url: 'http://localhost:4000',
-        description: 'Local server'
-      }
-    ],
-    components: {
-      securitySchemes: {
-        apiKey: {
-          type: 'apiKey',
-          name: 'apiKey',
-          in: 'header'
-        }
-      }
-    }
-  }
-};
-
-// Register Swagger
-server.register(swagger, swaggerOptions);
-
-// Register Swagger UI
-server.register(swaggerUi, {
-  customSiteTitle: 'Habit Tracker API Docs',
-  routePrefix: '/docs',
-  uiConfig: {
-    docExpansion: 'list',
-    deepLinking: false
-  },
-  staticCSP: true,
-  transformStaticCSP: (header) => header,
-  transformSpecification: (swaggerObject) => {
-    return swaggerObject;
-  },
-  transformSpecificationClone: true,
-});
 
 // Register Mongoose API
 server.register(fastifyMongooseAPI, {
