7c0a160754dc5e826c4335cf94c40896d9e5d297
Fernando JS Silva <ferjssilva@gmail.com>
2025-01-26 19:46:18 -0300

    refactor: remove unused API methods from User model

--- src/models/User.js
+++ src/models/User.js
@@ -10,25 +10,4 @@ const UserSchema = new Schema({
   },
 });
 
-UserSchema.methods.apiValues = async function (request) {
-  if (request?.body?.username) {
-    return {
-      username: request.body.username
-    };
-  } else {
-    throw new Error('GET is disabled');
-  }
-};
-
-// UserSchema.statics.apiPost = async function (body) {
-//   throw new Error('POST is disabled');
-// }
-UserSchema.methods.apiPut = async function (body) {
-  throw new Error('UPDATE is disabled');
-}
-
-UserSchema.methods.apiDelete = async function () {
-  throw new Error('DELETE is disabled');
-}
-
 export default mongoose.model('user', UserSchema);
