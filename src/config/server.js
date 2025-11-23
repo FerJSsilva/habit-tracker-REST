5ff5decb0eeadee2005a112ef568ab112e86b302
Fernando JS Silva <ferjssilva@gmail.com>
2025-11-23 04:38:15 -0300

    feat: update dependencies and improve logger configuration

--- src/config/server.js
+++ src/config/server.js
@@ -1,7 +1,8 @@
 import fastify from 'fastify';
 import fastifyFormbody from '@fastify/formbody';
+import loggerOptions from './logger.js';
 
-const server = fastify({ logger: false });
+const server = fastify({ logger: loggerOptions });
 
 server.register(fastifyFormbody);
 
